bbcbf4ef146d1a336645fe3be03e1d0ca262b908
Maksim Dmitrievich Borisov <burchik017@gmail.com>
2021-05-04 10:48:59 +0300

    push 2 tasks

---
+++ task2/task2.ts
@@ -0,0 +1,39 @@
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/typescript
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+/////// Assignment 
+// export function accum(s: string): string {
+// }
+///////example 
+
+/////// Solution
+
+export function accum(str: string): string | void {
+  if ( /^[a-zA-Z]+$/.test(str) ) {
+    let a : string = '';
+    for (let i = 0; i < str.length; i++) {
+      a += str[i].toUpperCase() + ((i > 0) ? str[i].repeat(i+0).toLowerCase() : '');
+      if (i !== str.length - 1) {
+        a += '-';
+      };
+    };
+    return a;
+  }
+}
+
+console.log(accum('asvrQW'));
+
+
+///////// for me
+// function accum(s: string): string {
+//   return s.split("").map((value, index) => (value.toUpperCase() + value.toLowerCase().repeat(index))).join("-");
+// }
